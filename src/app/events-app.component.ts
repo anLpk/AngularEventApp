023617ffff84e3bab446a20aed6b17eae3452ac6
Anıl Pak <71243066+anLpk@users.noreply.github.com>
2021-08-30 20:34:39 +1000

    angular project started

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: `
+    <events-list>
+      <nav-bar></nav-bar>
+    </events-list>
+  `
+})
+export class EventsAppComponent {
+
+}
